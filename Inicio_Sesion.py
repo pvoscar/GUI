49add43cf43fdd701aec92215d88ba6ce58497f4
OscarPV <pvoscarl@gmail.com>
2023-04-26 23:33:48 -0600

    Se agrega app Widgets

--- Inicio_Sesion.py
+++ Inicio_Sesion.py
@@ -17,35 +17,34 @@ def ingresar(*args):
         mess += "\nDatos ingresados exitosamente"
         messagebox.showinfo(message=mess, title="Mensaje Info")
     
-
-#Se crea el objeto ventana y se le agrega un título
+#Se crea la ventana raíz
 raiz = Tk()  
 raiz.title("Inicio de Sesión")
-
-#Se crea el marco principal y se posiciona de tal forma que abarque toda la pantalla
-marcoPrincipal = ttk.Frame(raiz, padding="15 15 15 15")
-marcoPrincipal.grid(column=0, row=0)
 
 #Se definen las variables a utilizar
 usuario = StringVar()
 contrasenya = StringVar()
 
-#Se agregan los cuadros de texto al marco principal
-txtUsuario = ttk.Entry(marcoPrincipal, textvariable=usuario, width=20, justify="center")
-txtUsuario.grid(column=1, row=0, columnspan=2)
-txtContrasenya = ttk.Entry(marcoPrincipal, textvariable=contrasenya, width=20, justify="center", show="*")
-txtContrasenya.grid(column=1, row=1, columnspan=2)
+#Se crea el marco principal y se posiciona de tal forma que abarque toda la pantalla
+marcoPrincipal = ttk.Frame(raiz, padding="15 15 15 15")
+marcoPrincipal.grid(column=0, row=0)
 
-#Se agregan las etiquetas al marco principal
-ttk.Label(marcoPrincipal, text="Usuario:").grid(column=0, row=0, sticky=W)   #objeto anónimo
-ttk.Label(marcoPrincipal, text="Contraseña:").grid(column=0, row=1, sticky=W)
+#Se configura el grid
+marcoPrincipal.columnconfigure(0, weight=1)
+marcoPrincipal.columnconfigure(1, weight=3)
 
-#Se agrega el botón al marco principal
-ttk.Button(marcoPrincipal, text="Ingresar", command=ingresar, width=8).grid(column=1, row=2, columnspan=2, sticky=E)
+#Se agregan los widgets de usuario al marco principal
+ttk.Label(marcoPrincipal, text="Usuario:").grid(column=0, row=0, sticky=E, padx=10, pady=10)
+txtUsuario = ttk.Entry(marcoPrincipal, textvariable=usuario)
+txtUsuario.grid(column=1, row=0, sticky=E, padx=10, pady=10)
 
-#Se recorre todos los widgets y se agrega espacio a la izquierda, derecha, arriba y debajo del widget
-for hijo in marcoPrincipal.winfo_children():
-    hijo.grid_configure(padx=7, pady=7)
+#Se agregan los widgets de Contraseña al marco principal
+ttk.Label(marcoPrincipal, text="Contraseña:").grid(column=0, row=1, sticky=E, padx=10, pady=10)
+txtContrasenya = ttk.Entry(marcoPrincipal, textvariable=contrasenya, show="*")
+txtContrasenya.grid(column=1, row=1, sticky=E, padx=10, pady=10)
+
+#Se agrega el botón Ingresar al marco principal
+ttk.Button(marcoPrincipal, text="Ingresar", command=ingresar).grid(column=1, row=3, sticky=E, padx=10, pady=10)
 
 #Se establece el objeto que tendrá el foco y captura el enter en la pantalla raíz
 txtUsuario.focus()  
